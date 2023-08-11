72318cf267fcf8b655319381eccaa7e024923dd2
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-08-11 23:32:09 +0700

    source code credit card html css

--- resources/js/Components/Error.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Error({ value, children }) {
-    return (
-        <div className="mt-2 text-rose-500 font-medium">
-            {value ? value : children}
-        </div>
-    );
-}
